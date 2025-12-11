e20b5a4b16b67e6753e095c05a30d311e7b0a9ad
Buga69 <bugalucian88@gmail.com>
2025-12-11 17:32:24 +0200

    Polished scrapper script, allowing it to accept as input a make and model. Fixed some small bugs

--- scripts/scrapper.py
+++ scripts/scrapper.py
@@ -5,12 +5,18 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import pandas as pd
+
+import os
 import csv
+
 import math
+import re
 
 import conversion
 
 def get_safely_data(driver, path):
+
+    #Get HTML header from given XPATH
     try:
         html_content = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, path)))
         return html_content.get_attribute("textContent")
@@ -19,6 +25,8 @@ def get_safely_data(driver, path):
 
 
 def get_links_on_page(make, model, page):
+
+    print("Currently looking for links on page: " + page)
 
     # Setup Chrome options for debugging (visible browser)
     chrome_options = Options()
@@ -31,11 +39,9 @@ def get_links_on_page(make, model, page):
     car_links = []
 
     try:
-        print("Opening browser - watch what happens...")
         driver.get("https://www.autovit.ro/autoturisme/" + make + "/" + model + "?page=" + page)
-
-        # input("Press Enter after the page loads completely...")
-
+        driver.implicitly_wait(10)
+        
         # Get all links on the page
         all_links = driver.find_elements(By.TAG_NAME, "a")
         print(f"Found {len(all_links)} total links on page")
@@ -58,6 +64,9 @@ def get_links_on_page(make, model, page):
     return car_links
 
 def get_links(make, model):
+
+    print("Looking for links for: " + make + model)
+
     chrome_options = Options()
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
@@ -67,6 +76,12 @@ def get_links(make, model):
 
     try:
         driver.get("https://www.autovit.ro/autoturisme/" + make + "/" + model)
+
+        if driver.current_url == "https://www.autovit.ro/autoturisme":
+            print("Unable to find any data on site about type of car")
+            driver.quit()
+            os._exit(1)
+        
         number_ads = get_safely_data(driver, "//div/p/b")
     except Exception as e:
         print(f"Error: {e}")
@@ -74,16 +89,16 @@ def get_links(make, model):
     finally:
         driver.quit()
 
-        time.sleep(1)
+        # Get number of pages for a type of car
         number_ads = conversion.str_to_int(number_ads)
         number_pages = math.floor(number_ads / 32) + 1 #32 is the number of ads per page 
         page = 1
 
         all_links = []
+        #For each page, find all the links from that page
         while page <= number_pages:
             all_links += get_links_on_page(make, model, str(page))
             page += 1
-            time.sleep(1)
         
         return all_links
 
@@ -94,7 +109,7 @@ def scrape_car_data(link):
     chrome_options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=chrome_options)
-    CAR = {
+    CAR = { #Data to scrape
         "Marca": "N/A",
         "Model": "N/A",
         "An fabricație": "N/A",
@@ -111,6 +126,7 @@ def scrape_car_data(link):
     try:
         driver.get(link)
 
+        #Scrape all required data from ad
         CAR["Marca"] = get_safely_data(driver, "//div[@data-testid='make']/div[1]/p")
 
         CAR["Model"] = get_safely_data(driver, "//div[@data-testid='model']/div[1]/p")
@@ -148,23 +164,40 @@ def scrape_car_data(link):
         return CAR
 
 def scrape_autovit(make, model):
+    #form csv data
     data_list = [ ]
-    data_csv = make + model + ".csv"
     fieldname = ["Marca", "Model", "An fabricație", "Preț", "Km", "Combustibil", 
                 "Tip Cutie de viteze", "Tip Caroserie",  "Capacitate Cilindrică", "Putere", "Link"]
 
-    links = get_links(make, model)
+    data_csv = make + model + ".csv"
+    output_directory = r".\data"
+    file_csv = os.path.join(output_directory, data_csv)
+
+    #find all links for type of car
+    links = get_links(make, model) 
+    
+    #for all links, get the data for each one
     for i, lnk in enumerate(links):
+        print("Currently looking at link: " + str(i))
         CAR = scrape_car_data( links[i] )
         data_list.append( CAR )
-        
-        time.sleep(1)
 
-    with open(data_csv, mode = 'w', newline='') as file:
+    #create csv file
+    with open(file_csv, mode = 'w', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=fieldname)
-        # writer.writeheader()
         writer.writerows(data_list)  
 
 if __name__ == "__main__":
-    scrape_autovit("dacia", "sandero")
+    make = input("Choose the make you wish to scrape: ")
+    model = input("Choose the model of the make you chose. If you wish all models, dont enter anything: ")
 
+    if make == "":
+        print("Must enter a make.")
+        exit(1)
+    
+    make = make.lower()
+    model = model.lower()
+    make = re.sub("[ &]", "-", make)
+    model = re.sub("[ &]", "-", model)
+
+    scrape_autovit(make, model)
